d921a44304f06d113e838475f022c3608fce8ddc
Chiara Mooney <34109996+chiaramooney@users.noreply.github.com>
2021-06-02 11:17:34 -0700

    Add SketchCanvas Back

--- src/RNGalleryList.ts
+++ src/RNGalleryList.ts
@@ -8,7 +8,7 @@ import {CheckBoxExamplePage} from './examples/CheckBoxExamplePage';
 import {ConfigExamplePage} from './examples/ConfigExamplePage';
 import {DatePickerExamplePage} from './examples/DatePickerExamplePage';
 import {TimePickerExamplePage} from './examples/TimePickerExamplePage';
-//import {SketchExamplePage} from './examples/SketchExamplePage';
+import {SketchExamplePage} from './examples/SketchExamplePage';
 import {SliderExamplePage} from './examples/SliderExamplePage';
 // Disabled from #121
 //import {PermissionsExamplePage} from './examples/PermissionsExamplePage';
@@ -144,10 +144,10 @@ export const RNGalleryList: Array<IRNGalleryExample> = [
     key: 'Slider',
     component: SliderExamplePage,
   },
-  /*{
+  {
     key: 'Sketch',
     component: SketchExamplePage,
-  },*/
+  },
   {
     key: 'Switch',
     component: SwitchExamplePage,
